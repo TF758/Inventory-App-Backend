a68947dd1c6e05bfb350de0345507da8e4f8583d
TF758 <teryllfelix@gmail.com>
2025-09-04 22:54:40 -0400

    created cusotm pagination  class to disable pagination at the url level

---
+++ db_inventory/pagination.py
@@ -0,0 +1,11 @@
+from rest_framework.pagination import PageNumberPagination
+
+class OptionalPagination(PageNumberPagination):
+    """
+    Allows disabling pagination by passing ?paginate=false in the query params.
+    """
+    def paginate_queryset(self, queryset, request, view=None):
+        paginate_param = request.query_params.get("paginate")
+        if paginate_param and paginate_param.lower() == "false":
+            return None  # disables pagination, returns full queryset
+        return super().paginate_queryset(queryset, request, view)
