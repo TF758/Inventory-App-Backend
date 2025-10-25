2adeadc32566bf6f5c87546688180a7e9a9c2a49
Teryll Felix <72235197+TF758@users.noreply.github.com>
2025-10-25 19:08:10 -0400

    update user write serializer to allow blank password fields

--- db_inventory/serializers/users.py
+++ db_inventory/serializers/users.py
@@ -22,14 +22,17 @@ class UserWriteSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
         required=False,
+        allow_blank=True,   # 👈 THIS IS THE FIX
         validators=[validate_password],
         style={'input_type': 'password'}
     )
-    confirm_password = serializers.CharField(  # 🔹 add confirmation field
+    confirm_password = serializers.CharField(  
         write_only=True,
         required=False,
+        allow_blank=True,   # 👈 SAME HERE
         style={'input_type': 'password'}
     )
+
 
     class Meta:
         model = User
@@ -61,11 +64,15 @@ class UserWriteSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
+        """
+        Update user fields. Password updates are handled separately.
+        """
+        validated_data.pop('password', None)
+        validated_data.pop('confirm_password', None)
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        if password:
-            instance.set_password(password)
+
         instance.save()
         return instance
 
