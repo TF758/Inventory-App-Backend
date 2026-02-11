f3562fbc30b6b021a3bd10180fe5d426d9057dee
Teryll Felix <72235197+TF758@users.noreply.github.com>
2026-02-10 21:39:28 -0400

    updated ScheduledTaskRun admin panel to provide gerater data layout

--- db_inventory/admin.py
+++ db_inventory/admin.py
@@ -13,7 +13,31 @@ from django.utils.translation import gettext_lazy as _
 
 admin.site.register(Notification)
 
-admin.site.register(ScheduledTaskRun)
+@admin.register(ScheduledTaskRun)
+class ScheduledTaskRunAdmin(admin.ModelAdmin):
+    list_display = (
+        "task_name",
+        "status",
+        "run_at",
+        "duration_ms",
+        "message",
+    )
+
+    list_filter = (
+        "status",
+        "task_name",
+    )
+
+    ordering = ("-run_at",)
+
+    readonly_fields = (
+        "task_name",
+        "status",
+        "run_at",
+        "duration_ms",
+        "schema_version",
+        "message",
+    )
 
 admin.site.register(EquipmentAssignment)
 
