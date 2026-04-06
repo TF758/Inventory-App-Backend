c460a3415dee931f019cab7cf123854da48d37af
Teryll Felix <72235197+TF758@users.noreply.github.com>
2026-04-06 15:16:57 -0400

    extract csv import functionality to pandas utility function removed download error report, asset reports now produce error sheet

--- data_import/import_urls.py
+++ data_import/import_urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from data_import.views import AssetImportCreateView, ImportErrorReportDownloadView
+from data_import.views import AssetImportCreateView
 
 
 
 urlpatterns = [
     path("asset-import/", AssetImportCreateView.as_view(), name="asset-import"),
-    path( "asset-import/<str:job_id>/errors/", ImportErrorReportDownloadView.as_view(), name="asset-import-errors", ),
 ]
