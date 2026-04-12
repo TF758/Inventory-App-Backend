aa6b92f3d63cb1a77f83b87e2c48b5fafa54037c
Teryll Felix <72235197+TF758@users.noreply.github.com>
2026-04-12 01:16:15 -0400

    refactor: improve JWT token handling and user assignment in middleware

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -1,28 +1,37 @@
 from urllib.parse import parse_qs
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
+from django.contrib.auth.models import AnonymousUser
 
 
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        scope = dict(scope) 
-
         scope["user"] = await self.get_anonymous_user()
 
-        query_string = scope.get("query_string", b"").decode()
-        params = parse_qs(query_string)
-        token = params.get("token")
+        token = None
+
+        # --- Preferred: Sec-WebSocket-Protocol ---
+        subprotocols = scope.get("subprotocols", [])
+        if len(subprotocols) == 2 and subprotocols[0] == "jwt":
+            token = subprotocols[1]
+
+        # --- Fallback: query string ---
+        if not token:
+            query_string = scope.get("query_string", b"").decode()
+            params = parse_qs(query_string)
+            token_list = params.get("token")
+            if token_list:
+                token = token_list[0]
 
         if token:
-            user = await self.get_user(token[0])
-            if user is not None:
+            user = await self.get_user(token)
+            if user:
                 scope["user"] = user
 
         return await super().__call__(scope, receive, send)
 
     @database_sync_to_async
     def get_anonymous_user(self):
-        from django.contrib.auth.models import AnonymousUser
         return AnonymousUser()
 
     @database_sync_to_async
@@ -30,7 +39,6 @@ class JWTAuthMiddleware(BaseMiddleware):
         from db_inventory.authentication import SessionJWTAuthentication
         from rest_framework_simplejwt.exceptions import InvalidToken
         from rest_framework.exceptions import AuthenticationFailed
-        from django.contrib.auth.models import AnonymousUser
 
         auth = SessionJWTAuthentication()
 
