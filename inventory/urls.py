0b02fbed28bd3e59a5d63ae1fc4afce44d530969
Teryll Felix <72235197+TF758@users.noreply.github.com>
2026-04-16 06:14:55 -0400

    added sites url to application

--- inventory/urls.py
+++ inventory/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('db_inventory.api_urls')),
-    path('reports/', include('reporting.urls.report_urls')),   # ← add this
+    path('reports/', include('reporting.urls.report_urls')),   
+    path('sites/', include('sites.urls.site_urls')),   
     path('imports/', include('data_import.import_urls')),
 
 ]
