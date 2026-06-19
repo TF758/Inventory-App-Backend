07533a87febf4b96970edbd0c53d3e43b0f7b3c4
Teryll Felix <72235197+TF758@users.noreply.github.com>
2026-06-18 22:21:25 -0400

    delete core permisison module
    permisison now ahndle entirely though autorization app which centrlaizies permission engine/logic

--- core/permissions/__init__.py
+++
@@ -1,5 +0,0 @@
-# Public permission classes for views
-from .assets import AssetPermission, CanManageAssetCustody, CanUpdateEquipmentStatus
-from .users import UserPermission, RolePermission, UserPlacementPermission, FullUserCreatePermission
-from .helpers import *
-from .constants import ROLE_HIERARCHY
